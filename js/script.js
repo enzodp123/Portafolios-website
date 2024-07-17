738376012f49bfa770bf17be5f8a27f05d567a3a
enzodp123 <108824262+enzodp123@users.noreply.github.com>
2024-07-17 17:20:59 -0300

    commit de scroll

--- js/script.js
+++ js/script.js
@@ -7,3 +7,19 @@ window.addEventListener("scroll", ()=>{
     span.classList.toggle("animation-span", window.scrollY > 300)
     span2.classList.toggle("scroll", window.scrollY > 650)
 })
+
+//scroll JQUERY
+
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+            window.location.hash = hash;
+        });
+    }
+    });
+});
