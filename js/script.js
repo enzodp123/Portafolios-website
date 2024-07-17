702224f45276fd570c0d4f9fddba18a5776b084e
enzodp123 <108824262+enzodp123@users.noreply.github.com>
2024-07-16 23:46:45 -0300

    my second commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 let header = document.querySelector(".header");
 let span = document.querySelector(".span");
+let span2 = document.querySelector(".span-2");
 
 window.addEventListener("scroll", ()=>{
     header.classList.toggle("blur", window.scrollY > 3);
-    span.classList.toggle("animation-span", window.scrollY > 100)
+    span.classList.toggle("animation-span", window.scrollY > 300)
+    span2.classList.toggle("scroll", window.scrollY > 650)
 })
